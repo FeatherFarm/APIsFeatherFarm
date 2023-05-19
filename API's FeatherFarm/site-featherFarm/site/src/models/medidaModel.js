7df0e5123d601ba1d3b8b06127742877e02c2ccd
mazzoni2005 <murilo.rsantos@sptech.school>
2023-05-19 20:37:23 -0300

    site com api

---
+++ API's FeatherFarm/site-featherFarm/site/src/models/medidaModel.js
@@ -0,0 +1,66 @@
+var database = require("../database/config");
+
+function buscarUltimasMedidas(fkSensor, limite_linhas) {
+
+    instrucaoSql = ''
+
+    if (process.env.AMBIENTE_PROCESSO == "producao") {
+        instrucaoSql = `select top ${limite_linhas}
+        dht11_temperatura as temperatura, 
+        dht11_umidade as umidade,  
+                        momento,
+                        FORMAT(momento, 'HH:mm:ss') as momento_grafico
+                    from medida
+                    where fk_aquario = ${idAquario}
+                    order by id desc`;
+    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+        instrucaoSql = `select 
+        temperatura as temperatura, 
+        umidade as umidade,
+                        data_hora,
+                        DATE_FORMAT(data_hora,'%H:%i:%s') as momento_grafico
+                    from medida
+                    where fkSensor = 1 order by data_hora desc limit 5`;
+    } else {
+        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+        return
+    }
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function buscarMedidasEmTempoReal(fkSensor) {
+
+    instrucaoSql = ''
+
+    if (process.env.AMBIENTE_PROCESSO == "producao") {
+        instrucaoSql = `select top 1
+        dht11_temperatura as temperatura, 
+        dht11_umidade as umidade,  
+                        CONVERT(varchar, momento, 108) as momento_grafico, 
+                        fk_aquario 
+                        from medida where fk_aquario = ${idAquario} 
+                    order by id desc`;
+
+    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+        instrucaoSql = `select 
+        temperatura as temperatura, 
+        umidade as umidade,
+                        DATE_FORMAT(data_hora,'%H:%i:%s') as momento_grafico, 
+                        fkSensor 
+                        from medida where fkSensor = 1 order by data_hora desc limit 5`;
+    } else {
+        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+        return
+    }
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+
+module.exports = {
+    buscarUltimasMedidas,
+    buscarMedidasEmTempoReal
+}
